8d807b99d07081d09c9c836269735f26d85f0ba8
Justin Gwinn <gwinnj3@gmail.com>
2025-03-05 23:15:41 -0600

    idk

---
+++ app/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    
+      <p>foot</p>
+    
+  );
+};
+
+export default Footer;
---
+++ app/Header.tsx
@@ -0,0 +1,10 @@
+
+const Header: React.FC = () => {
+  return (
+    
+      <h1>My React App</h1>
+    
+  );
+};
+
+export default Header;
